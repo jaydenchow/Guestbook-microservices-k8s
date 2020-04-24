8fb6ce231b7bcb37934f3ebfe6ea3e8a7fb140dc
jaydenchow <56620215+jaydenchow@users.noreply.github.com>
2020-04-24 20:44:28 +0800

    Update deployToK8s.sh

--- guestbook-service/deployToK8s.sh
+++ guestbook-service/deployToK8s.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl delete -f ../kube-deploy/guestbook.yaml
-kubectl create -f ../kube-deploy/guestbook.yaml
+kubectl delete -f ./guestbook.yaml
+kubectl create -f ./guestbook.yaml
